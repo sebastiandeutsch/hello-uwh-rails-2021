9c81c6cf445eded294d9b2b4040d8c13f94101f5
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2021-11-20 11:21:20 +0100

    CRUD posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -16,6 +20,34 @@ class PostsController < ApplicationController
     else
       flash.alert = 'Something went wrong'
       render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash.notice = "Post updated"
+      redirect_to posts_path
+    else
+      flash.alert = 'Could not update Post'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash.notice = "Post deleted"
+      redirect_to posts_path
+    else
+      flash.alert = "Could not delete Post"
+      redirect_to posts_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'posts' => 'posts#index'
-  get 'posts/new' => 'posts#new'
-  post 'posts' => 'posts#create'
+  resources :posts
 end
